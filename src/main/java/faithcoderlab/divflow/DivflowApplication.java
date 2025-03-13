bf43adac7ce2fc516fe5888993c687ea37d72f3f
FaithCoderLab <seinplan97@gmail.com>
2025-03-14 00:03:54 +0900

    initial commit

---
+++ src/main/java/faithcoderlab/divflow/DivflowApplication.java
@@ -0,0 +1,13 @@
+package faithcoderlab.divflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faithcoderlab/divflow/DivflowApplicationTests.java
@@ -0,0 +1,13 @@
+package faithcoderlab.divflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
